5fed90ce59ca3ad128a7157349407574bd1a32d9
Kseniia Nikolaienko <ksysha.nikolaenko778@gmail.com>
2024-03-07 16:07:29 +0200

    fixes

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -7,13 +7,15 @@ import { CoreModule } from 'src/app/core/core.module'
 import { LayoutModule } from 'src/app/shared/modules/layout/layout.module'
 
 import { HomeRoutingModule } from 'src/app/modules/home/home-routing.module'
-import { HomeContactsComponent } from 'src/app/modules/home/pages/home/components/home-contacts/home-contacts.component'
-import { HomeHeroComponent } from 'src/app/modules/home/pages/home/components/home-hero/home-hero.component'
-import { HomeModalComponent } from 'src/app/modules/home/pages/home/components/home-modal/home-modal.component'
-import { HomePresentationComponent } from 'src/app/modules/home/pages/home/components/home-presentation/home-presentation.component'
-import { HomeStageComponent } from 'src/app/modules/home/pages/home/components/home-stage/home-stage.component'
-import { HomeStagesComponent } from 'src/app/modules/home/pages/home/components/home-stages/home-stages.component'
-import { HomeSubscriptionComponent } from 'src/app/modules/home/pages/home/components/home-subscription/home-subscription.component'
+import {
+  HomeContactsComponent,
+  HomeHeroComponent,
+  HomeModalComponent,
+  HomePresentationComponent,
+  HomeStageComponent,
+  HomeStagesComponent,
+  HomeSubscriptionComponent,
+} from 'src/app/modules/home/pages/home/components'
 import { HomeComponent } from 'src/app/modules/home/pages/home/home.component'
 
 @NgModule({
---
+++ src/app/modules/home/pages/home/components/index.ts
@@ -0,0 +1,13 @@
+export * from './home-contacts/home-contacts.component'
+
+export * from './home-hero/home-hero.component'
+
+export * from './home-modal/home-modal.component'
+
+export * from './home-presentation/home-presentation.component'
+
+export * from './home-stage/home-stage.component'
+
+export * from './home-stages/home-stages.component'
+
+export * from './home-subscription/home-subscription.component'
